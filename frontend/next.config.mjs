30031e4c4f850db1c6fd1ccd01532b9ac858ca7c
Vincent Fearing <vincefearing@gmail.com>
2024-09-19 02:03:22 -0700

    Fixed deployment issues for github pages

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  assetPrefix: "/eventease/",
+  basePath: "/eventease",
   images: {
     unoptimized: true,
   },
