a5096d215195abbda9167f2ae732fd37a823363c
Vincent Fearing <vincefearing@gmail.com>
2024-09-19 02:23:29 -0700

    pathing error fix

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  assetPrefix: "/eventease/",
-  basePath: "/eventease",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/eventease/" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/eventease" : "",
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 export default nextConfig;
